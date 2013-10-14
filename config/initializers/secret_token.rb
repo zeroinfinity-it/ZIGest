b18792fd5c996aa28a4970539c0e6d99b2e403c4
zeroinfinity <gianni.codevico@zeroinfinity.it>
2013-10-14 10:57:55 +0200

    Git inizializzato

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZIGest::Application.config.secret_key_base = '948bbb73e2992e4ea40225f9e0eb038d85a4f56c2c566abe24791a612975803aa5ad97dfe963bda19af08a89c45d6927c7b78e22470adbb15f795d230d12c2af'
